81315bff3efb6b4ed76a3110892cefe6de2aeefa
caugustoarruda <caugustogarruda@gmail.com>
2020-12-22 12:40:47 -0300

    Deleting category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -15,6 +15,25 @@ router.get('/admin/categories', (req, res) => {
     })
 });
 
+router.post('/categories/delete', (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect('/admin/categories')
+            })
+        }else{
+            res.redirect('/admin/categories')
+        }
+    }else{
+        res.redirect('/admin/categories')
+    }
+});
+
 router.post('/categories/save', (req, res) => {
     var title = req.body.title;
     if(title != undefined){
