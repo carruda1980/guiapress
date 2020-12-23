de8209d39462ec31aca58a403ccae6a8d7404991
caugustoarruda <caugustogarruda@gmail.com>
2020-12-23 09:42:38 -0300

    Saving changes on edit form

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -66,4 +66,20 @@ router.get('/admin/categories/edit/:id', (req, res) => {
     }
 });
 
+router.post('/categories/update', (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({
+        title: title,
+        slug: Slugify(title)
+    }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect('/admin/categories')
+    })
+});
+
 module.exports = router
