d3a7066d685953e22cc1b4f34e5f50f3886dc97d
caugustoarruda <caugustogarruda@gmail.com>
2020-12-23 10:01:40 -0300

    Articles form

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/articles', (req, res) => {
-    res.send("Hello from articles!")
+router.get('/admin/articles/new', (req, res) => {
+    res.render('admin/articles/new')
 });
 
 module.exports = router
