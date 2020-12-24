67b150011a3a28181555c933dda8044e4d0d977a
caugustoarruda <caugustogarruda@gmail.com>
2020-12-24 13:34:10 -0300

    Reading article

--- index.js
+++ index.js
@@ -40,6 +40,23 @@ app.get('/', (req, res)=> {
     })
 });
 
+app.get('/:slug', (req, res) => {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            res.render('article', {
+                article: article
+            })
+        }else {
+            res.redirect('/')
+        }
+    })
+})
+
 app.listen(8080, () =>{
     console.log("App started")
 });
