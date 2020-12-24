d8a05a55d1fa5095c79415c05e25e05815d7f847
caugustoarruda <caugustogarruda@gmail.com>
2020-12-24 14:12:55 -0300

    Filter by category

--- index.js
+++ index.js
@@ -61,7 +61,33 @@ app.get('/:slug', (req, res) => {
             res.redirect('/')
         }
     })
-})
+});
+
+app.get('/category/:slug', (req, res) => {
+    var slug = req.params.slug;
+
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{
+            model: Article
+        }]
+    }).then(category => {
+        if(category != undefined){
+            Category.findAll().then(categories => {
+                res.render('index', {
+                    articles: category.articles,
+                    categories: categories
+                })
+            })
+        }else {
+            res.redirect('/')
+        }
+    }).catch(err => {
+        res.redirect('/');
+    })
+});
 
 app.listen(8080, () =>{
     console.log("App started")
