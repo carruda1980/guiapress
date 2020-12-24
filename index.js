3287c722ba9429e19b03a38bf4785f0d679843dc
caugustoarruda <caugustogarruda@gmail.com>
2020-12-24 13:56:03 -0300

    Dynamic homenavbar

--- index.js
+++ index.js
@@ -34,9 +34,12 @@ app.use('/', articlesController);
 
 app.get('/', (req, res)=> {
     Article.findAll().then(articles => {
-        res.render("index.ejs", {
-            articles: articles
-        });
+        Category.findAll().then(categories => {
+            res.render("index", {
+                articles: articles,
+                categories: categories
+            });
+        })
     })
 });
 
@@ -48,8 +51,11 @@ app.get('/:slug', (req, res) => {
         }
     }).then(article => {
         if(article != undefined){
-            res.render('article', {
-                article: article
+            Category.findAll().then(categories => {
+                res.render("article", {
+                    article: article,
+                    categories: categories
+                });
             })
         }else {
             res.redirect('/')
