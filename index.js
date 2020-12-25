b85f340ede3dfd9bfb3ab28680764b57ff3d6167
caugustoarruda <caugustogarruda@gmail.com>
2020-12-25 14:36:06 -0300

    Users model/controller

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const connection = require('./database/database');
 
 const categoriesController = require('./categories/CategoriesController');
 const articlesController = require('./articles/ArticlesController');
+const usersController = require('./users/UsersController');
 
 // Import Models
 const Category = require('./categories/Category');
 const Article = require('./articles/Article');
+const User = require('./users/User');
 
 // View engine
 app.set('view engine', 'ejs');
@@ -31,6 +33,7 @@ connection.authenticate().then(() =>{
 
 app.use('/', categoriesController);
 app.use('/', articlesController);
+app.use('/', usersController);
 
 app.get('/', (req, res)=> {
     Article.findAll().then(articles => {
